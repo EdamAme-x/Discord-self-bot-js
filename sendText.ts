1337dd586bf8421a49d8e830cb3135682310c7b0
EdamAme <121654029+EdamAme-x@users.noreply.github.com>
2023-12-15 23:08:41 +0900

    fix

--- sendText.ts
+++ sendText.ts
@@ -2,15 +2,15 @@ import { MessageDto } from "npm:discord-gateways";
 
 export async function sendText(
   text: string,
-  message: MessageDto,
+  message: MessageDto
 ): Promise<boolean> {
   const response = await fetch(
     "https://discord.com/api/v9/channels/" + message.channel_id + "/messages",
     {
-      "headers": {
-        "accept": "*/*",
+      headers: {
+        accept: "*/*",
         "accept-language": "ja,en-US;q=0.9,en;q=0.8",
-        "authorization": Deno.env.get("token") ?? "",
+        authorization: Deno.env.get("token") ?? "",
         "content-type": "application/json",
         "sec-ch-ua":
           '"Not_A Brand";v="8", "Chromium";v="120", "Google Chrome";v="120"',
@@ -23,13 +23,17 @@ export async function sendText(
         "x-discord-locale": "ja",
         "x-discord-timezone": "Asia/Tokyo",
       },
-      "referrerPolicy": "strict-origin-when-cross-origin",
-      "body": '{"mobile_network_type":"unknown","content":"' +
-        encodeURIComponent(text) + '","tts":false,"flags":0}',
-      "method": "POST",
-      "mode": "cors",
-      "credentials": "include",
-    },
+      referrerPolicy: "strict-origin-when-cross-origin",
+      body: JSON.stringify({
+        mobile_network_type: "unknown",
+        content: text,
+        tts: false,
+        flags: 0,
+      }),
+      method: "POST",
+      mode: "cors",
+      credentials: "include",
+    }
   );
 
   if (!response.ok) {
