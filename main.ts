44e87e4ffd7bde6fcbd3f3476a33a94fc5eea44e
Ame_x <121654029+EdamAme-x@users.noreply.github.com>
2023-12-15 22:26:31 +0900

    Update main.ts

--- main.ts
+++ main.ts
@@ -1,75 +1,12 @@
-import { WebSocket } from 'npm:ws';
-import { EventEmitter } from 'npm:events';
+import { DiscordClient, MessageDto } from 'discord-gateways';
 
-export declare interface DiscordClient {
-    on(event: 'messageCreate', listener: (message: any) => void): this;
-}
+const client = new DiscordClient(Deno.env.get("token") ?? "");
 
-export class DiscordClient extends EventEmitter {
-    private discordToken: string;
-    private seq: number | null;
-    private session_id: string | null;
-    private ack: boolean;
-    private heartbeatTimer: NodeJS.Timer | undefined;
-    private ws: WebSocket;
+client.on("messageCreate", (message: MessageDto) => {
+    console.log(message);
+});
 
-    constructor(discordToken: string) {
-        super();
-        this.discordToken = discordToken;
-        this.seq = null;
-        this.session_id = null;
-        this.ack = false;
-        this.ws = new WebSocket('wss://gateway.discord.gg/?encoding=json&v=9');
-    }
-
-    public connect() {
-        this.ws = new WebSocket('wss://gateway.discord.gg/?encoding=json&v=9');
-
-        this.ws.on('message', (data: string) => {
-            const payload = JSON.parse(data);
-            const { op, d, s, t } = payload;
-
-            this.seq = s ? s : this.seq;
-
-            console.log(payload);
-        })
-    }
-
-    private heartbeat() {
-        this.ws.send(JSON.stringify({
-            'op': 1,
-            'd': this.seq
-        }));
-        this.ack = false;
-
-        setTimeout(() => {
-            if (!this.ack) {
-                this.ws.close();
-                this.ack = false;
-                if (this.heartbeatTimer) {
-                    clearInterval(this.heartbeatTimer);
-                }
-                this.connect();
-            }
-        }, 5000);
-    }
-
-    private identify() {
-        this.ws.send(JSON.stringify({
-            'op': 2,
-            'd': {
-                'token': this.discordToken,
-                'properties': {
-                    '$os': 'linux',
-                    '$browser': 'chrome',
-                    '$device': 'chrome'
-                }
-            }
-        }));
-    }
-}
-
-new DiscordClient(Deno.env.get("AccountToken"))
+client.connect();
 
 Deno.serve(() => {
   return new Response("WebSocket Only")
